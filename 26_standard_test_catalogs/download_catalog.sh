3f156c6fd85f0120e5927d4b5a71550c182a988c
flgomezc <fl.gomez10@uniandes.edu.co>
2019-08-27 16:40:07 -0500

    Download AbacusCosmos_720box_planck FOF halos @ z = 0.1

---
+++ 26_standard_test_catalogs/download_catalog.sh
@@ -0,0 +1,1 @@
+wget http://nbody.rc.fas.harvard.edu/public/AbacusCosmos/AbacusCosmos_720box_planck_products/AbacusCosmos_720box_planck_00-0_products/AbacusCosmos_720box_planck_00-0_FoF_halos/z0.100/halos.tar.gz
