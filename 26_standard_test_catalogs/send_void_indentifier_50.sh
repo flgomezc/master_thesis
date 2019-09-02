a3db68d35aec236cb65a35def8174e429a35c4ca
Felipe Leonardo Gomez-Cortes <fl.gomez10@uniandes.edu.co>
2019-09-02 15:53:02 -0500

    Slurm sbatch scripts

---
+++ 26_standard_test_catalogs/send_void_indentifier_50.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=Vcirmax50            #Nombre del job
+#SBATCH -p medium			#Cola a usar, Default=short (Ver colas y límites en /hpcfs/shared/README/partitions.txt)
+#SBATCH -N 1				#Nodos requeridos, Default=1
+#SBATCH -n 1				#Tasks paralelos, recomendado para MPI, Default=1
+#SBATCH --cpus-per-task=1		#Cores requeridos por task, recomendado para multi-thread, Default=1
+#SBATCH --mem-per-cpu=4048		#Memoria en Mb por CPU, Default=2048
+#SBATCH --time=159:00:00		#Tiempo máximo de corrida, Default=2 horas. Formato HH:MM:SS
+#SBATCH --mail-user=fl.gomez10@uniandes.edu.co
+#SBATCH --mail-type=ALL			
+#SBATCH -o Vcircmax_50_.o%j.LOG	#Nombre de archivo de salida
+
+host=`/bin/hostname`
+date=`/bin/date`
+echo "BetaSkeletonVoidFinder en HPC Magnus"
+echo "Corri en la maquina: "$host
+echo "Corri el: "$date
+
+module load anaconda/python3.7
+
+make main_sequence FILENUM=50
