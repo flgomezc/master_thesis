4f9d7af703dfbf04af1916032dcb386d43dd63e2
Felipe Leonardo Gomez-Cortes <fl.gomez10@uniandes.edu.co>
2019-09-03 12:46:21 -0500

    Testing over a grid of parameters beta and nrand, from 1.0 to 2.0 using steps of 0.2

--- 26_standard_test_catalogs/beta_nrand_grid_generator.py
+++ 26_standard_test_catalogs/beta_nrand_grid_generator.py
@@ -1,4 +1,9 @@
+#!/lustre/apps/anaconda/3.7/bin/python3
+# Runing on the Magnus Cluster.
+# Be sure to load the anaconda/python3.7 module.
+
 import numpy as np
+import subprocess as subp
 
 beta  = np.linspace(1,2,6)
 nrand = np.linspace(1,2,6)
@@ -6,6 +11,8 @@ nrand = np.linspace(1,2,6)
 counter = 0
 for b in beta:
     for n in nrand:
-        print(b, n, counter)
+        #print(b, n, counter)      
+        process_string = "make main_sequence FILENAME=v300.dat FILENUM={} BETA={} NRAND={}".format(counter, b, n)
+        subp.run(process_string , shell=True, check=True)
         counter +=1
 
